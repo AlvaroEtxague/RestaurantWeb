d4e60c72644865ea9804a56f516a829caed5cc55
Tinowski <alvaroetxague@gmail.com>
2020-11-10 18:27:22 +0000

    fixing css

--- js/app.js
+++ js/app.js
@@ -1,6 +1,7 @@
 const burgerIcon = document.querySelector('.toggle-burger-icon');
 const nav = document.querySelector('.nav-links');
-
+const mainSection = document.querySelector('.main-section');
 burgerIcon.addEventListener('click', () => {
   nav.classList.toggle('active');
+  mainSection.classList.toggle('mutate');
 });
